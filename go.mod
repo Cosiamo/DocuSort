e366df688b040cfca195c22724bdd5b40a4752ec
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-04-12 14:08:07 -0500

    updated main package

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/Cosiamo/DocuSort
 
 go 1.17
 
-require github.com/Cosiamo/DocuSort/dsx v0.0.0-20220411175825-df7d8e21c05b
+require github.com/Cosiamo/DocuSort/dsx v0.0.0-20220412172731-4b68bd3b0810
 
 require (
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
@@ -11,7 +11,7 @@ require (
 	github.com/xuri/efp v0.0.0-20220407160117-ad0f7a785be8 // indirect
 	github.com/xuri/excelize/v2 v2.6.0 // indirect
 	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
-	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
-	golang.org/x/net v0.0.0-20220407224826-aac1ed45d8e3 // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -6,36 +6,51 @@ import (
 	"github.com/Cosiamo/DocuSort/dsx"
 )
 
+// The main func for DocuSort serves as testing for dsx,
+// as well as, showing examples for GitHub
 func main() {
-	f := dsx.OpenXlsx("spreadsheets/testSheet.xlsx")
-	defer dsx.CloseXlsx(f)
+	// open files
+	file := dsx.OpenXlsx("spreadsheets/testSheet.xlsx")
+	// closes files
+	defer dsx.CloseXlsx(file)
+	// which sheet the method's access
 	sheet := "Sheet1"
 
-	// var testCol []string = []string{"A", "B", "C", "D"}
-	// // var testRow []int = []int{2, 3, 4, 5, 6}
+	var testCol0 []string = []string{"A", "B", "C", "D"}
+	var testRow0 []int = []int{2, 3, 4, 5, 6}
+	// group data from specific columns
+	// dsx.Group(f *excelize.File, sheet string, columns []string, rows []int) chan string
+	for test0 := range dsx.Group(file, sheet, testCol0, testRow0) {
+		fmt.Println(test0)
+	}
 
-	// startRow := 2
-	// endRow := 6
-	// // returns int slices within params passed into the function
-	// for testRow := range dsx.RangeOfRows(startRow, endRow) {
-	// 	// returns values of cells from an array of strings and ints
-	// 	for test0 := range dsx.Group(f, sheet, testCol, testRow) {
-	// 		fmt.Println(test0)
-	// 	}
-	// }
+	startRow1 := 1
+	endRow1 := 30
+	// returns int slices within params passed into the function
+	// dsx.RangeOfRows(startRow int, endRow int) chan []int
+	for test1 := range dsx.RangeOfRows(startRow1, endRow1) {
+		fmt.Println(test1)
+	}
 
-	
+	startCol2 := "A"
+	endCol2 := "BZ"
+	// returns string slices within params passed into the function
+	// dsx.RangeOfCols(startCol string, endCol string) chan []string
+	for test2 := range dsx.RangeOfCols(startCol2, endCol2) {
+		fmt.Println(test2)
+	}
 
-	// // returns all data from the spreadsheet by column
-	// for test2 := range dsx.AllDataByCols(f, sheet) {
-	// 	fmt.Print(test2)
-	// 	fmt.Println()
-	// }
-
-	// returns all data from the spreadsheet by row
-	for test3 := range dsx.AllDataByRows(f, sheet) {
+	// returns all data from the spreadsheet by column
+	// dsx.AllDataByCols(f *excelize.File, sheet string) chan string
+	for test3 := range dsx.AllDataByCols(file, sheet) {
 		fmt.Print(test3)
 		fmt.Println()
 	}
 
+	// returns all data from the spreadsheet by row
+	// dsx.AllDataByRows(f *excelize.File, sheet string) chan string
+	for test4 := range dsx.AllDataByRows(file, sheet) {
+		fmt.Print(test4)
+		fmt.Println()
+	}
 }
